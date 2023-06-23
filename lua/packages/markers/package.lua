a0fb8291b621366bb75a389f08308a68b8539057
PrikolMen <olegis019@gmail.com>
2023-06-23 22:18:58 +0400

    New gpm version

--- lua/packages/markers/package.lua
+++ lua/packages/markers/package.lua
@@ -1,7 +1,7 @@
 name = "markers"
 version = 010101
 autorun = true
-logger = true
-
-cl_main = "cl_init.lua"
-main = "init.lua"
+init = {
+    ["client"] = "cl_init.lua",
+    ["server"] = "init.lua"
+}
